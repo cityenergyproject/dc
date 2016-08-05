6d586384d6fa4917dcaaa9c2113574dce64af042
gcorradini <gregcorradini@gmail.com>
2016-08-05 15:52:22 -0700

    fix address search double event registration

--- src/app/router.js
+++ src/app/router.js
@@ -93,7 +93,7 @@ define([
     },
     onChange: function(){
       var changed = _.keys(this.state.changed);
-      if (_.contains(changed, 'url_name') || _.contains(changed, 'year')){
+      if (_.contains(changed, 'url_name')){
         this.onDataSourceChange();
       }
       this.navigate(this.state.toUrl(), {trigger: false, replace: true});
