ec828ee857540a0f8b3cc1b1bec113d5362064cc
gcorradini <gregcorradini@gmail.com>
2016-08-29 08:10:52 -0700

    add doee data scripts

---
+++ doee_scripts/geocode_doee.py
@@ -0,0 +1,81 @@
+import psycopg2
+from psycopg2 import pool as psycopg2_pool
+from optparse import make_option
+from multiprocessing import pool as multiproc_pool
+import time
+import os,sys,string
+import logging
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG )
+logger=logging.getLogger(__name__)
+from decimal import Decimal, getcontext
+getcontext().prec = 11
+from geopy.geocoders import GeocoderDotUS, Nominatim, GoogleV3, GeoNames
+from geopy.exc import GeocoderTimedOut
+from contextlib import contextmanager
+
+YOUR_GOOGLE_API_KEY=''
+geolocator = GoogleV3(api_key=YOUR_GOOGLE_API_KEY) #GeoNames() Nominatim() 
+
+# create pool with min number of connections of 1, max of 15
+dbpool = psycopg2_pool.SimpleConnectionPool(1,30,dbname=os.environ["DBNAME"],user=os.environ['DBUSER'])
+
+tablename = os.environ["TABLE"]
+
+@contextmanager
+def get_connection():
+    conn = dbpool.getconn()
+    conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    try:
+        yield conn
+    finally:
+        dbpool.putconn(conn)
+
+def geocode(prop):
+    attrmap = {'pid': 0, 'address': 1, 'city': 2, 'state': 3, 'zip': 4, 'ward': 5 }
+    full_address = "{} {}, {} {}".format(
+        prop[attrmap['address']],
+        prop[attrmap['city']] or '',
+        prop[attrmap['state']] or '',
+        prop[attrmap['zip']] or '',
+    )
+    if 'None' in full_address:
+        logger.debug("[ SKIP ]: {}".format(full_address)) 
+
+    try:
+        location = geolocator.geocode(full_address)
+        if location is None:
+           logger.debug("[ NO LOCATION SKIP ]: {}".format(full_address))
+           return  
+        logger.debug("[ GEOCODED ]: {}".format(location))
+        with get_connection() as conn:
+            crs = conn.cursor()
+            crs.execute("UPDATE "+tablename+" SET the_geom = ST_SetSRID(ST_Point(%s, %s),4326), is_geocoded = 1 WHERE pid = %s",(location.longitude,location.latitude,prop[attrmap['pid']]))
+    except GeocoderTimedOut as ge: # GeocoderTimedOut
+        logger.exception( ge ) 
+    except Exception as e: 
+        logger.exception( e ) 
+
+
+if __name__ == '__main__':
+    with get_connection() as conn:
+        thread_pool = multiproc_pool.ThreadPool(processes=15)
+
+        #
+        # geocode tablename with an `address_of_record` 
+        #
+        crs = conn.cursor()
+        crs.execute("SELECT pid, address_of_record, city, state, postal_code, ward FROM "+tablename+" WHERE address_of_record IS NOT NULL AND reported_address IS NULL AND needs_geocoding = 1 and is_geocoded = 0")
+        props1 = crs.fetchall()
+
+        #
+        # geocode tablename with no `address_of_record` but a `reported_address` 
+        #
+        crs.execute("SELECT pid, reported_address, city, state, postal_code, ward from "+tablename+" WHERE reported_address IS NOT NULL AND needs_geocoding = 1 and is_geocoded = 0")
+        props2 = crs.fetchall()
+
+        thread_pool.imap(
+            geocode,
+            (prop for prop in props1+props2)
+        )   
+        thread_pool.close()
+        thread_pool.join() # block until finished
---
+++ doee_scripts/load_doee.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+set -o errexit #abort if any command fails
+#
+#
+# 1. to run, make sure this script is in the same directory
+#    as the yearly CSV(s) you'll want to load
+#
+# 2. then cd to that directoy
+#
+# 3. change the environment variables to match your system and run:
+#
+# DBNAME=<your-database-name> \
+# TABLE=<database-tablename> \
+# CSVNAME=<name-of-csv> \
+# DBUSER=<your-db-username> ./load_doee.sh
+#
+#
+
+WDIR=$(pwd)
+if [ -z ${DBNAME} ] && [ -z ${DBUSER} ] && [ -z ${TABLE} ] && [ -z ${CSVNAME} ]; then echo "you need to set environment variables 'DBNAME','DBUSER','TABLE', 'CSVNAME'"; exit 1; else echo "DBNAME='$DBNAME' DBUSER='$DBUSER' TABLE='$TABLE' CSVNAME='$CSVNAME'"; fi
+DBNAME=${DBNAME}
+DBUSER=${DBUSER}
+TABLENAME=${TABLE}
+CSVFULLPATH=$WDIR/${CSVNAME}.csv
+if [ ! -f $CSVFULLPATH ]; then echo "the file '$CSVFULLPATH' does not exist"; exit 1; fi
+
+#  DOEE "should" be sending 
+#  pipe-delimited CSV(s) with data in the schema and column order
+#  defined by the CREATE TABLE statement below
+#  swap out their headers for those defined in the application
+replacement_headers="pid|dc_real_pid|pm_pid|property_name|pm_parent_pid|parent_property_name|year_ending|report_status|address_of_record|owner_of_record|ward|reported_address|city|state|postal_code|year_built|primary_ptype_self|primary_ptype_epa|tax_record_floor_area|reported_gross_floor_area|energy_star_score|site_eui|weather_norm_site_eui|source_eui|weather_norm_source_eui|total_ghg_emissions|total_ghg_emissions_intensity|water_use|electricity_use|natural_gas_use|fuel_use|district_water_use|metered_areas_energy|metered_areas_water|xcoordinate|ycoordinate|ycorrected|xcorrected"
+sed -i '1s/.*/'"${replacement_headers}"'/'  $CSVFULLPATH;
+
+# load
+psql_load="
+CREATE TABLE ${TABLENAME}(
+    pid VARCHAR (255),
+    dc_real_pid VARCHAR (255),
+    pm_pid VARCHAR (255),
+    property_name VARCHAR (255),
+    pm_parent_pid VARCHAR (255),
+    parent_property_name VARCHAR (255),
+    year_ending VARCHAR (255),
+    report_status VARCHAR (255),
+    address_of_record VARCHAR (255),
+    owner_of_record VARCHAR (255),
+    ward VARCHAR (255),
+    reported_address VARCHAR (255),
+    city VARCHAR (255),
+    state VARCHAR (255),
+    postal_code VARCHAR (255),
+    year_built INTEGER,
+    primary_ptype_self VARCHAR (255),
+    primary_ptype_epa VARCHAR (255),
+    tax_record_floor_area DOUBLE PRECISION,
+    reported_gross_floor_area DOUBLE PRECISION,
+    energy_star_score INTEGER,
+    site_eui DOUBLE PRECISION,
+    weather_norm_site_eui DOUBLE PRECISION,
+    source_eui DOUBLE PRECISION,
+    weather_norm_source_eui DOUBLE PRECISION,
+    total_ghg_emissions DOUBLE PRECISION,
+    total_ghg_emissions_intensity DOUBLE PRECISION,
+    water_use DOUBLE PRECISION,
+    electricity_use DOUBLE PRECISION,
+    natural_gas_use DOUBLE PRECISION,
+    fuel_use DOUBLE PRECISION,
+    district_water_use DOUBLE PRECISION,
+    metered_areas_energy VARCHAR (255),
+    metered_areas_water VARCHAR (255),
+    xcoordinate DOUBLE PRECISION,
+    ycoordinate DOUBLE PRECISION,
+    ycorrected DOUBLE PRECISION,
+    xcorrected DOUBLE PRECISION
+);
+COPY ${TABLENAME} FROM '$CSVFULLPATH' DELIMITER '|' CSV HEADER;"
+psql -U $DBUSER -d $DBNAME -c "DROP TABLE IF EXISTS ${TABLENAME}"
+psql -U $DBUSER -d $DBNAME -c "$psql_load"
+
+
+psql_transform="
+ALTER TABLE ${TABLENAME} ADD COLUMN the_geom geometry(Point,4326);
+ALTER TABLE ${TABLENAME} ADD COLUMN needs_geocoding INTEGER DEFAULT 0;
+ALTER TABLE ${TABLENAME} ADD COLUMN is_geocoded INTEGER DEFAULT 0;
+UPDATE ${TABLENAME} set the_geom = ST_SetSRID(ST_Point(xcoordinate, ycoordinate),4326) WHERE xcoordinate IS NOT NULL AND ycoordinate IS NOT NULL;
+UPDATE ${TABLENAME} set the_geom = ST_SetSRID(ST_Point(xcorrected, ycorrected),4326) WHERE xcorrected IS NOT NULL AND ycorrected IS NOT NULL;
+UPDATE ${TABLENAME} set needs_geocoding = 1 WHERE the_geom IS NULL;
+UPDATE ${TABLENAME} SET address_of_record = NULL WHERE address_of_record LIKE ANY ('{\"Exempt%\",\"%Report%\",\"%Compliance%\"}');
+
+-- TODO: they should be doing this error checking before handing us the data
+-- DOEE defined outliers that should be NULLIFIED
+UPDATE ${TABLENAME} SET 
+    site_eui = NULL, 
+    weather_norm_site_eui = NULL, 
+    source_eui = NULL,
+    weather_norm_source_eui = NULL,
+    energy_star_score = NULL,
+    total_ghg_emissions = NULL,
+    total_ghg_emissions_intensity = NULL, 
+    water_use = NULL,
+    electricity_use = NULL,
+    natural_gas_use = NULL,
+    fuel_use = NULL,
+    district_water_use = NULL
+WHERE weather_norm_source_eui > 1000 AND energy_star_score > 5;"
+psql -U $DBUSER -d $DBNAME -c "$psql_transform"
