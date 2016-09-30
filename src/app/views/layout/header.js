4b6523bd7681f65671f3dc67c3e1e973e8edb62a
Sean Connelley <connelley@stamen.com>
2016-09-30 01:31:08 -0400

    Break up header logos and add links, #34

--- src/app/views/layout/header.js
+++ src/app/views/layout/header.js
@@ -17,10 +17,11 @@ define([
       var city = this.state.get('city'),
           name = city.get('name'),
           url_name = city.get('url_name'),
-          link_url = city.get('logo_link_url');
+          logo_link = city.get('logo_link_url'),
+          banner_images = city.get('header_banner_images');
       document.title = name;
 
-      $('#title').html(this.template({url_name: url_name, title: name, link_url: link_url}));
+      $('#title').html(this.template({url_name: url_name, title: name, logo_link: logo_link, banner_images: banner_images}));
       return this;
     }
   });
