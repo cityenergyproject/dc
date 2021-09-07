ba1778d14341806dc0deb867fd239960780e3950
Oleksandr Rastvorov <oleksandr.rastvorov@codicetech.com>
2021-09-07 11:58:28 +0300

    feat: DWEM-21 Show all options for category filters

---
+++ src/app/models/category_filter_model.js
@@ -0,0 +1,20 @@
+define([
+  'backbone',
+  'app/collections/category_filters.js',
+], function(Backbone, CategoryFilterCollection) {
+  var CategoryFiltersModel = Backbone.Model.extend({
+    initialize: function(options) {
+      var categoryFilters = options.categoryFilterFields.reduce(function(acc, field) {
+        acc[field] = new CategoryFilterCollection({
+          tableName: options.tableName,
+          cartoDbUser: options.cartoDbUser,
+          field: field
+        }).fetch();
+        return acc;
+      }, {});
+      this.set(categoryFilters);
+    }
+  });
+
+  return CategoryFiltersModel;
+});
