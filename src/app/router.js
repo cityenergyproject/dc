57482af36d24a38d72cb13135ddcf14b9d31d13e
Andrii Volosovich <audrey.volosovich@codicetech.com>
2021-09-20 23:27:07 +0300

    DWEM-40 Remove dead code

--- src/app/router.js
+++ src/app/router.js
@@ -15,7 +15,6 @@ define([
   'views/map/address_search_autocomplete',
   'views/map/year_control',
   'views/layout/building_counts',
-  'views/building_comparison/building_comparison',
   'views/layout/compare_bar',
   'views/layout/activity_indicator',
   'views/scorecards/controller',
@@ -26,7 +25,7 @@ define([
 ], function($, deparam, _, Backbone, CityModel, ScorecardModel, CategoryFiltersModel,
             CityBuildings, HeaderView, FooterView, MapView,
             AddressSearchView, YearControlView, BuildingCounts,
-            BuildingComparisonView, CompareBar, ActivityIndicator,
+            CompareBar, ActivityIndicator,
             ScorecardController, MobileAlert, ModalModel,
             ModalController, Landing) {
 
@@ -161,7 +160,6 @@ define([
       var yearControlView = new YearControlView({state: this.state});
       var mapView = new MapView({state: this.state});
       var addressSearchView = new AddressSearchView({mapView: mapView, state: this.state});
-      // var comparisonView = new BuildingComparisonView({state: this.state}); // TODO dead code, should be removed
       var compareBar = new CompareBar({state: this.state});
       var buildingsCounts = new BuildingCounts({state: this.state});
       var footerView = new FooterView({state: this.state});
