76bdca09474c027393c5eeb49a0176bf413caf14
BenchmarkDC <32399536+BenchmarkDC@users.noreply.github.com>
2023-02-09 11:54:10 -0500

    Update category_filters.js

--- app/collections/category_filters.js
+++ app/collections/category_filters.js
@@ -4,7 +4,7 @@ define([
 ], function(_, Backbone) {
 
   var urlTemplate = _.template(
-    "https://<%= cartoDbUser %>.carto.com/api/v2/sql"
+    "https://gcp-us-east1.api.carto.com/v3/sql/carto_dw/query?q="
   );
 
   var CategoryFilterCollection = Backbone.Collection.extend({
@@ -12,16 +12,17 @@ define([
       this.tableName = options.tableName;
       this.cartoDbUser = options.cartoDbUser;
       this.field = options.field;
+      this.token = options.token;
     },
     url: function() {
       return urlTemplate(this);
     },
     toSql: function() {
-      return 'SELECT Distinct ' + this.field + ' as value FROM '+ this.tableName
+      return 'SELECT Distinct ' + this.field + ' as value FROM carto-dw-ac-f61is3yf.shared.'+ this.tableName
     },
     fetch: function() {
       var query = this.toSql();
-      Backbone.Collection.prototype.fetch.apply(this, [{ data: { q: query } }]);
+      Backbone.Collection.prototype.fetch.apply(this, [{ headers: {'Authorization' :'Bearer'+this.token, data: { q: query } }]);
       return this.models;
     },
     parse: function(data){
