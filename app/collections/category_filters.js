40bc29323681439f17ba68d978cc47df5e7a08eb
BenchmarkDC <32399536+BenchmarkDC@users.noreply.github.com>
2023-02-22 16:24:50 -0500

    Update category_filters.js

--- app/collections/category_filters.js
+++ app/collections/category_filters.js
@@ -4,10 +4,7 @@ define([
 ], function(_, Backbone) {
 
   var urlTemplate = _.template(
-    "https://gcp-us-east1.api.carto.com/v3/sql/carto_dw/query"
-  );
-  var urlAuth = _.template(
-    "https://auth.carto.com/oauth/token"
+    "https://<%= cartoDbUser %>.carto.com/api/v2/sql"
   );
 
   var CategoryFilterCollection = Backbone.Collection.extend({
@@ -16,30 +13,15 @@ define([
       this.cartoDbUser = options.cartoDbUser;
       this.field = options.field;
     },
-    auth: function(){
-      return urlAuth(this);
-    },
-    authorization: function(){
-      Backbone.Collection.prototype.fetch.apply(this, [{headers:{'content-type': 'application/x-www-form-urlencoded'}, data:{
-        'grant_type':'client_credentials'
-        ,'client_id':'JIvxwJMeic2KezrNKd5ZC9BP40aPavDW'
-        ,'client_secret':'Run5xT50QJpy-fcRQmiohZ4K-7MvcnO7Pv5Gvf-30ysufSWK7kpK7mD2-XVw0kJd'
-        ,'audience':'carto-cloud-native-api'}}]);
-      return results;
-    },
-    parse_auth: function(data){
-      this.token = data.access_token;
-    },
     url: function() {
       return urlTemplate(this);
     },
     toSql: function() {
-      return 'SELECT Distinct ' + this.field + ' as value FROM carto-dw-ac-f61is3yf.shared.'+ this.tableName
+      return 'SELECT Distinct ' + this.field + ' as value FROM '+ this.tableName
     },
-    
     fetch: function() {
       var query = this.toSql();
-      Backbone.Collection.prototype.fetch.apply(this, [{ headers: {'Authorization' :'Bearer '+this.token}, data: { q: query } }]);
+      Backbone.Collection.prototype.fetch.apply(this, [{ data: { q: query } }]);
       return this.models;
     },
     parse: function(data){
@@ -49,4 +31,3 @@ define([
 
   return CategoryFilterCollection;
 });
-
